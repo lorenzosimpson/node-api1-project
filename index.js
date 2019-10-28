73f0c2f08f9076ab07242513bbc5fbd4299ffb4e
Lorenzo Simpson <lorenzosimp@gmail.com>
2019-10-28 15:56:02 -0400

    first commit, creates successful get request at api/users

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 // implement your API here
+const express = require('express');
+const db = require('./data/db');
+
+const server = express();
+server.use(express.json());
+
+server.get('/api/users', (req, res) => {
+    db.find()
+    .then(users => res.status(200).json(users))
+    .catch(err => console.log(err))
+})
+
+
+const port = 8000;
+server.listen(port, () => console.log(`\n=== Server listening on port ${port} ===\n`))

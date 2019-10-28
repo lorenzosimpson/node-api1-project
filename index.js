b0cda7f6cb4c19d8e1828be18a5967b5ac8b8259
Lorenzo Simpson <lorenzosimp@gmail.com>
2019-10-28 16:39:39 -0400

    creates post to /api/users, not finished

--- index.js
+++ index.js
@@ -5,12 +5,26 @@ const db = require('./data/db');
 const server = express();
 server.use(express.json());
 
+// GET users
 server.get('/api/users', (req, res) => {
     db.find()
     .then(users => res.status(200).json(users))
     .catch(err => {
         console.log(err)
         res.status(500).json({ error: 'failed to retrieve users'})
+    })
+})
+
+server.post('/api/users', (req, res) => {
+    const userObj = req.body;
+    db.insert(userObj)
+    .then(user => {
+        console.log(userObj)
+        !userObj.name || !userObj.bio ? res.status(400).json({ error: 'Please include name and bio'}) : res.status(201).json(user.id);
+    })
+    .catch(err => {
+        console.log(err)
+        res.status(500).json({ error: 'failed to create user'})
     })
 })
 
